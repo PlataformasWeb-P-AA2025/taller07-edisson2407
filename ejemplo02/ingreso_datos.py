b661fbd5f06807563d91ed5a24e63b776cf76a6b
edisson2407 <chambaedisson01@gmail.com>
2025-05-20 10:18:49 -0500

    tallersem07

--- ejemplo02/ingreso_datos.py
+++ ejemplo02/ingreso_datos.py
@@ -13,51 +13,35 @@ from configuracion import cadena_base_datos
 # sqlite
 engine = create_engine(cadena_base_datos)
 
+#conexion a la base de datos
 Session = sessionmaker(bind=engine)
 session = Session()
 
-# se crea un objetos de tipo Club 
-club1 = Club(nombre="Barcelona", deporte="Fútbol", \
-        fundacion=1920)
+#se lee y guarda los clubes desde el archivo txt.
+with open("/home/edisson/Documentos/web/sem07/taller07-edisson2407/ejemplo02/data/datos_clubs.txt", "r", encoding="utf-8") as f:
+        for l in f:
+                nombre, deporte, fundacion = l.strip().split(";")
+                club = Club(nombre=nombre, deporte=deporte, fundacion=int(fundacion))
+                session.add(club)
+session.commit() #cconfirmacion de cambios para que los clubes esten disponibles
 
-club2 = Club(nombre="Emelec", deporte="Fútbol", \
-        fundacion=1930)
+#se traen todos los clubes para relacionarlos con los jugadores
+clubes = {c.nombre: c for c in session.query(Club).all()}
 
-club3 = Club(nombre="Liga de Quito", deporte="Fútbol", \
-        fundacion=1940)
+with open("/home/edisson/Documentos/web/sem07/taller07-edisson2407/ejemplo02/data/datos_jugadores.txt", "r", encoding="utf-8") as f:
+        for l in f:
+                try:
+                        club_nombre, posicion, dorsal, nombre = l.strip().split(";")
+                        jugador = Jugador (
+                                nombre=nombre,
+                                dorsal=int(dorsal),
+                                posicion=posicion,
+                                club=clubes[club_nombre] #uso del nombre del club como llave
+                                )
+                        session.add(jugador)
+                except:
+                        pass
 
-# Se crean objeto de tipo Jugador
-#
-jugador1 = Jugador(nombre ="Damian Diaz", dorsal=10, posicion="mediocampo", \
-        club=club1)
-jugador2 = Jugador(nombre ="Matias Oyola", dorsal=18, posicion="mediocampo", \
-        club=club1)
-jugador3 = Jugador(nombre ="Dario Aymar", dorsal=2, posicion="defensa", \
-        club=club1)
-
-
-jugador4 = Jugador(nombre ="Oscar Bagui", dorsal=6, posicion="defensa", \
-        club=club2)
-jugador5 = Jugador(nombre ="Romario Caicedo", dorsal=11, posicion="mediocampo", \
-        club=club2)
-
-
-jugador6 = Jugador(nombre ="Adrián Gabbarini", dorsal=1, posicion="arquero", \
-        club=club3)
-jugador7 = Jugador(nombre ="Cristian Martinez", dorsal=9, posicion="delantero", \
-        club=club3)
-
-# se agrega los objetos
-# a la sesión
-session.add(club1)
-session.add(club2)
-session.add(club3)
-session.add(jugador1)
-session.add(jugador2)
-session.add(jugador3)
-session.add(jugador4)
-session.add(jugador5)
-session.add(jugador6)
 
 # se confirma las transacciones
 session.commit()
